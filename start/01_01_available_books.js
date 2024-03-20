3dd8ba39990c633c193d99385ea28df945558c37
Datasciencewebdevelopmentcoder <154837114+Datasciencewebdevelopmentcoder@users.noreply.github.com>
2024-03-20 15:43:52 +0000

    added javascript 01 - needs testing

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,24 @@
 // Write your code here - Kasey Nelson
+function Book(Title, Author, ISBN, numCopies ){
+  this.Title = Title
+  this.Author = Author
+  this.ISBN = ISBN
+  this.numCopies = numCopies
+}
+Book.prototype.getAvailablity = function(){
+  if (this.numCopies == 0){
+    return "out of stock"
+  }
+  else if (this.numCopies < 10){
+    return "Low stock"
+  }
+  else{
+    return "In stock"
+  }
+}
+Book.prototype.restock = function(numCopiesStocked = 5){
+  this.numCopies += numCopiesStocked
+}
+Book.prototype.sell = function(numCopiesSold = 1){
+  this.numCopies -= numCopiesSold
+}
